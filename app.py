bf282b603991cc9456cef7825d766f0f66a739e5
Ananya <100335251+ananyaknr@users.noreply.github.com>
2024-05-20 20:58:35 +0700

    Update app.py

--- app.py
+++ app.py
@@ -55,7 +55,8 @@ if image_file is not None:
     pred_class, class_num, prob = learn_inf.predict(image)
 
     display_pred_class = display_name_map.get(pred_class, pred_class)    
-    st.write("Prediction:",str(float(max(prob))),  display_pred_class)
+    st.write("Prediction:",  display_pred_class)
+    st.write(str(float(max(prob))))
 
     class_description = dscrp_map.get(pred_class, pred_class)    
     st.write("Description:", class_description)
